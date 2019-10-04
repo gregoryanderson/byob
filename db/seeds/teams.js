e88e125c8576ffa5ec3fcfc89ab7b15a662fd44e
Gregory Anderson <anderson.gregory.mail@gmail.com>
2019-10-04 09:45:09 -0600

    Add heroku

--- db/seeds/teams.js
+++ db/seeds/teams.js
@@ -1,47 +1,103 @@
 const csv = require("csv-parser");
 const fs = require("fs");
 const results = [];
+const playersArray = [];
+const statsArray = []
 
-fs.createReadStream("smallData.csv")
+fs.createReadStream("playerStats.csv")
   .pipe(csv())
-  .on("data", data => results.push(data))
-  .on("end", () => console.log(results));
+  //look up what data as string is
+  .on("data", data => {if(!playersArray.find(obj => obj.Player === data.Player)){
+    playersArray.push(data)
+  }
+    statsArray.push(data)
+  })
+  .on("end", () => console.log("parsing complete"));
 
-const createPlayer = (knex, player) => {
-  return knex("allPlayers")
-    .insert(
-      {
-        player: player.Player
-      },
-      "id"
-    )
-    .then(playerId => {
-      let statsPromises = {
-        year: player.Year,
-        assists: player.AST,
-        points: player.PTS,
-        player_id: playerId[0]
-      };
-      return createStats(knex, statsPromises);
-    });
-};
+const findOrCreatePlayer = (knex, rowOfStats) => {
+  // let player = knex('allPlayers').where('player', rowOfStats.Player)
+  // let morePlayer = player._statements.filter(data => data.value)
+  // let name = morePlayer[0].value
+  // console.log('this is the name', name)
+
+  // let potato = knex('allPlayers').whereNotExists(function() {
+  //   this.select('*').from('allPlayers').whereRaw('allPlayers.player = rowOfStats.Player');
+  // })
+
+  // console.log(potato)
+
+  // if (playerNameArray.includes(rowOfStats.Player)) {
+  //   // console.log(`your player is ${rowOfStats.Player}`);
+  //   let stats = {
+  //     year: rowOfStats.Year,
+  //     assists: rowOfStats.AST,
+  //     points: rowOfStats.PTS,
+  //     player_id: playerObjArray.find(player => {
+  //       if (true){
+  //         console.log('my guy', player.name)
+  //         return player.id 
+  //       }
+  //     })
+  //   };
+  //   return createStats(knex, stats);
+
+  // } else {
+    // playerNameArray.push(rowOfStats.Player)
+    // console.log("you are making a new player");
+    return knex("allPlayers").insert({
+      player: rowOfStats.Player
+    })
+  }
+
+      // .insert(
+      //   {
+      //     player: rowOfStats.Player
+      //   },
+      //   "id"
+      // )
+
+      // .then(playerId => {
+      //   let stats = {
+      //     year: rowOfStats.Year,
+      //     assists: rowOfStats.AST,
+      //     points: rowOfStats.PTS,
+      //     player_id: playerId[0]
+      //   };
+
+        // let playerObj = {
+        //   name: rowOfStats.Player,
+        //   id: playerId[0]
+        // }
+
+        // playerObjArray.push(playerObj)
+        // return createStats(knex, stats);
+//       });
+//   }
+// };
 
 const createStats = (knex, statObj) => {
-  return knex('stats').insert(statObj);
+  return knex('allPlayers').where('player', statObj.Player).first()
+  return knex("stats").insert(statObj);
 };
 
 exports.seed = knex => {
   return knex("stats")
-    .del() 
+    .del()
     .then(() => knex("allPlayers").del())
     .then(() => {
       let playerPromises = [];
 
-      results.forEach(player => {
-        playerPromises.push(createPlayer(knex, player));
+      playersArray.forEach(row => {
+        playerPromises.push(findOrCreatePlayer(knex, row));
       });
-
+      // console.log('the obj array', playerObjArray)
       return Promise.all(playerPromises);
+    }).then(() => {
+      let statsPromises = [];
+      statsArray.forEach(stat => {
+        statsPromises.push(createStats(knex, stat))
+      })
+      return Promise.all(statsPromises)
     })
     .catch(error => console.log(`Error seeding data: ${error}`));
 };
--- knexfile.js
+++ knexfile.js
@@ -46,4 +46,13 @@ module.exports = {
   //   }
   // }
 
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  }
+
 };
